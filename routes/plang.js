04e69ad1f7925260f1b1a50db92566f6b384a153
Ramon Brand <ramon2009@gmail.com>
2017-10-10 20:11:26 +0300

    Edited deployment.

--- routes/plang.js
+++ routes/plang.js
@@ -3,6 +3,10 @@ var router = express.Router();
 var fs = require('fs');
 
 /* GET home page. */
+router.get('/', function(req, res, next) {
+	res.render('plang/index.pug', { title: 'PLang Documentation' });
+});
+
 router.get('/:page', function(req, res, next) {
 	if(!fs.existsSync('./views/plang/' + req.params.page + '.pug')){
 		res.render('plang/invalidreq.pug', { title: 'PLang Documentation' });
